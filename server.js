2e0c10c3bbba3ab8198758c7ad7830fb9ebe2b4e
Chirag Goel <56836113+garihc23@users.noreply.github.com>
2023-10-16 16:51:27 +0530

    project commit

---
+++ server.js
@@ -0,0 +1,19 @@
+console.log("EXPRRESS PROJECT")
+const express = require("express");
+const connectDb = require("./config/dbConnection.js");
+const errorHandler = require("./middleware/errorHandler.js");
+const dotenv = require("dotenv").config();
+
+connectDb();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes.js"));
+app.use("/api/users", require("./routes/userRoutes.js"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server runnin on port ${port}`)
+});
