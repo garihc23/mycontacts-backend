3da6ff0a7fa07859f0d197cd5beb3a6a8823a293
Chirag Goel <56836113+garihc23@users.noreply.github.com>
2023-10-31 10:41:48 +0530

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 console.log("EXPRRESS PROJECT")
 const express = require("express");
+const cors = require('cors');
 const connectDb = require("./config/dbConnection.js");
 const errorHandler = require("./middleware/errorHandler.js");
 const dotenv = require("dotenv").config();
 
+
 connectDb();
 const app = express();
 
-const port = process.env.PORT || 5000;
+// Use CORS middleware
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your frontend URL
+  }));
 
+const port = process.env.PORT || 5000;
+  
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes.js"));
 app.use("/api/users", require("./routes/userRoutes.js"));
